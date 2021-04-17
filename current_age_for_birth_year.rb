12857714f71ee429239134450d2d8f2765aae8d3
Raghda  Muayad <therose000777@gmail.com>
2021-04-17 04:57:26 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,4 @@
-def current_age_for_birth_year(birth_year)
-  Time.now.year - birth_year
-end
-require_relative '../current_age_for_birth_year.rb'
+
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
